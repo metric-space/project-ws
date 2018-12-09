441dcc324d8832f01972fd232b01c645e30a8827
luke <nerorevenge@gmail.com>
2018-12-08 19:00:08 -0500

    Cleanup

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,15 @@
 module Main where
 
 import Prelude
-import Data.Array ((!!), length, mapWithIndex)
-import Data.Maybe (fromJust, maybe)
-import Data.Traversable
-import Graphics.Canvas (getCanvasElementById, getCanvasHeight, getCanvasWidth, getContext2D, setCanvasHeight, setCanvasWidth, Context2D, Rectangle(..), setFillStyle, fillRect, fillText, strokeRect)
-import Partial.Unsafe (unsafePartial)
-import Data.Int (toNumber)
-
-
-import Effect (Effect)
-import Effect.Console (log)
+import Data.Array         ((!!), length, mapWithIndex)
+import Data.Int           (toNumber)
+import Data.Maybe         (fromMaybe, Maybe(..))
+import Data.Traversable   (traverse_)
+import Effect             (Effect)
+import Effect.Console     (log)
+import Graphics.Canvas    (getCanvasElementById, getContext2D, setCanvasHeight, 
+                           setCanvasWidth, Context2D, Rectangle(..), 
+                           setFillStyle, fillRect, fillText, strokeRect, CanvasElement)
 
 
 play_map :: Array (Array Int)
@@ -50,14 +49,29 @@ block_height :: Int
 block_height = 30
 
 
--- make new type to hold information, plain Rectangle isn't cutting it
+string_to_color :: String -> String
+string_to_color "4" = "red"
+string_to_color "1" = "blue"
+string_to_color "2" = "yellow"
+string_to_color "3" = "green"
+string_to_color _   = "white"
+
+
 type NuRectangle = { r :: Rectangle , colour  :: String }
 
 
 to_nurect :: Int -> Int -> Int -> Int -> Int ->  NuRectangle
 to_nurect i j x bw bh = { r: rectangle, colour: (show x)} 
-                         where rectangle :: Rectangle 
-                               rectangle = {x: (toNumber (i*bw)), y: (toNumber (j*bh)), width: (toNumber bw), height: (toNumber bh)}
+                         where x_coord :: Number
+                               x_coord = toNumber (i*bw)
+
+                               y_coord :: Number
+                               y_coord =  toNumber (j*bh)
+
+                               rectangle :: Rectangle 
+                               rectangle = {x: x_coord, y: y_coord, 
+                                            width: (toNumber bw), 
+                                            height: (toNumber bh)}
 
 
 -- big fat code smell here
@@ -69,14 +83,6 @@ play_map_to_nurect_array block_w block_h play_map =
         inner_func index array_ = mapWithIndex (\j x -> to_nurect index j x block_w block_h) array_ 
                                                         
         
-string_to_color :: String -> String
-string_to_color "4" = "red"
-string_to_color "1" = "blue"
-string_to_color "2" = "yellow"
-string_to_color "3" = "green"
-string_to_color _   = "white"
-
-
 render_nu_rect :: Context2D -> NuRectangle -> Effect Unit
 render_nu_rect ctx r = do 
                          setFillStyle ctx (string_to_color r.colour)
@@ -90,17 +96,22 @@ render_play_map :: Context2D -> Array (Array NuRectangle) -> Effect Unit
 render_play_map ctx x = traverse_ (traverse_ (render_nu_rect ctx)) x  
  
 
+get_crackin :: CanvasElement -> Int -> Int -> Effect Unit
+get_crackin canvas w h  = do
+    let play_map_ = play_map_to_nurect_array block_width block_height play_map
+    ctx <- getContext2D canvas
+    _ <- setCanvasWidth canvas (toNumber w)
+    _ <- setCanvasHeight canvas (toNumber h)
+
+    render_play_map ctx play_map_
+
+
 main :: Effect Unit
 main = do
   let w = (length $ play_map) * block_width
-      h =  (length (unsafePartial (fromJust $ (play_map !! 0)))) * block_height
-  mcanvas <- getCanvasElementById "canvas"
-  -- fuck the unsafePartial stuff here, it's as bad as clojurescript's nil bs
-  -- cleanup after
-  let canvas = unsafePartial (fromJust mcanvas)
-      play_map_ = play_map_to_nurect_array block_width block_height play_map
-  ctx <- getContext2D canvas
-  _ <- setCanvasWidth canvas (toNumber w)
-  _ <- setCanvasHeight canvas (toNumber h)
+      h = (length <<< fromMaybe [] $ (play_map !! 0)) * block_height
+  canvas <- getCanvasElementById "canvas" 
+  case canvas of Nothing -> log "Canvas element not found!! check ID!!"
+                 Just c -> get_crackin c w h 
 
-  render_play_map ctx play_map_
+  
